718048a6548efb6918f8652f62b85605a5fcabed
lucascompython <lucasdelinhares@gmail.com>
2022-06-03 10:38:48 +0100

    Finish part 2

--- 2019/Solutions/day01.py
+++ 2019/Solutions/day01.py
@@ -2,7 +2,23 @@ with open ("../Inputs/2019_01.txt") as f:
     input_data = f.readlines()
 
 total_fuel = 0
+# PART 1
 for i in input_data:
     fuel = int(i) // 3 - 2
+
     total_fuel += fuel
+    
+print(total_fuel)
+
+# PART 2
+total_fuel = 0
+for i in input_data:
+    fuel = int(i) // 3 - 2
+    
+    while not fuel <= 0 :
+        total_fuel += fuel
+        fuel = fuel // 3 - 2
+        print(fuel)
+
+
 print(total_fuel)
