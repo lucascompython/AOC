a32534ca488aea6747a2a544685e9f4d82902b5f
Lucas de Linhares <lucasdelinhares@gmail.com>
2026-03-16 14:00:07 +0000

    solve day 1

--- 2025/src/bin/01.rs
+++ 2025/src/bin/01.rs
@@ -1,7 +1,24 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let mut current: i64 = 50;
+    let mut count = 0;
+    for line in input.lines() {
+        let op = line.chars().next()?;
+        let num = line[1..].parse::<i64>().ok()?;
+
+        match op {
+            'R' => current = (current + num) % 100,
+            'L' => current = (current + 100 - num) % 100,
+            _ => return None,
+        };
+
+        if current == 0 {
+            count += 1;
+        }
+    }
+
+    Some(count)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
